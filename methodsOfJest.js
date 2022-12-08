79b3fe43d98627b102d975767230b765f460967c
mohamed <m635379@gmail.com>
2022-12-08 03:41:19 +0200

    learn onley, skip, beforeAll, afterAll and beforeEach methods

--- methodsOfJest.js
+++ methodsOfJest.js
@@ -22,3 +22,29 @@
     test(name, func, timeout)
     test(name, func, timeout)
 })*/
+
+// test.onley() just run test has onley
+// describe.onley() just run describe has onley
+
+// test.skip() just stop test has skip
+// describe.skip() just stop describe has skip
+
+// timeout = 5s
+
+// beforeAll(func, timeout) run before all tests
+/* 
+beforeAll(() => {
+    #conect to database
+    #empty testing table
+    #add dummy data for testing
+    #prepare everything before testing
+})
+*/
+// afterAll(func, timeout) run after all tests
+/* 
+afterAll(() => {
+    #clean database
+    #clean cache
+})
+*/
+// beforeEach(func, timeout) run before one mark test
